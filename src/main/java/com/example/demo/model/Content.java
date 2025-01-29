e27528f371b63eafb7da6615745df4b0382b4b09
Hanna Friberg <friberg.hanna@outlook.com>
2025-01-29 16:04:17 +0100

    added getters & setters to Content model class

--- src/main/java/com/example/demo/model/Content.java
+++ src/main/java/com/example/demo/model/Content.java
@@ -14,4 +14,44 @@ public class Content {
     private String fileName;
     private String filePath;
 
+    public Content() {
+    }
+
+    public Content(String fileName, String filePath) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    @Override
+    public String toString() {
+        return "Content{" +
+                "id=" + id +
+                ", fileName='" + fileName + '\'' +
+                ", filePath='" + filePath + '\'' +
+                '}';
+    }
 }
