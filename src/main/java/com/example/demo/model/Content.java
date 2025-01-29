8f1c6a41e983b45e8d1e6a1dce319730f4da8418
Hanna Friberg <friberg.hanna@outlook.com>
2025-01-29 10:23:37 +0100

    content model setup

--- src/main/java/com/example/demo/model/Content.java
+++ src/main/java/com/example/demo/model/Content.java
@@ -2,9 +2,15 @@ package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Content {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+    private String filePath;
 
 }
