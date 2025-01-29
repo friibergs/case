a66f9ff1923b556bb60a18cf16034f4c7a471e3d
Hanna Friberg <friberg.hanna@outlook.com>
2025-01-29 14:55:11 +0100

    code clean up

--- src/main/java/com/example/demo/model/Content.java
+++ src/main/java/com/example/demo/model/Content.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
